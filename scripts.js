783b93ccd4e3f6738c2c8d045836828a3d195e35
Nikki Consolacion <nikkiconsolacion@gmail.com>
2019-07-27 11:56:37 -0700

    2 approaches to addEventListener

--- scripts.js
+++ scripts.js
@@ -1,34 +1,56 @@
 const listContainer = document.querySelector('#list-container');
-const destroyButton = document.querySelectorAll('button');
 
 let customers = [
   { id: 1, name: 'moe', email: 'moe@gmail.com', isVIP: true},
   { id: 2, name: 'larry', isVIP: true, email: 'larry@gmail.com'},
   { id: 4, name: 'shep', email: 'shep@gmail.com'},
  ];
+ function destroy() {
+  const destroyButton = document.querySelectorAll('.destroyButton');
+
+  destroyButton.forEach( item => {
+    //console.log(item.button)
+    item.addEventListener('click', ev => {
+      console.log('click')
+    })
+
+  })
+  // destroyButton.addEventListener('click', (ev) => {
+  //   if (ev.target.tagName === 'LI') {
+  //     customers = customers.filter((customer, _idx)=> {
+  //       const idx = [...ev.target.parentNode.parentNode.children].indexOf(ev.target);
+  //       return _idx !== idx;
+  //     })
+  //     render();
+  //   }
+  //   // else {
+
+  //   // }
+
+  // })
+ }
 
 
 const render = () => {
-  const html = customers.map(customer => {
+  const html = customers.forEach(customer => {
     let customerInfo = customer.name + " " + customer.email;
     console.log(customerInfo);
-    return `<li>${customerInfo}<button>Destroy</button></li>`
-  }).join('');
-  listContainer.innerHTML = html;
+    const li = document.createElement('li');
+    li.innerHTML = `${customerInfo}<button class="destroyButton">Destroy</button>`;
+    console.log('lastChild here', li.lastChild);
 
-destroyButton.addEventListener('click', ev => {
-  if (ev.target.tagName === 'LI') {
-    customers = customers.filter((customer, _idx)=> {
-      const idx = [...ev.target.parentNode.children].indexOf(ev.target);
-      return idx !== _idx;
+    li.lastChild.addEventListener('click', ev => {
+
     })
-    render();
-  }
-  // else {
 
-  // }
+    listContainer.appendChild(li);
 
-})
+    return li;
+    //return `<li>${customerInfo}<button>Destroy</button></li>`
+  });
+  //listContainer.innerHTML = html;
+
 }
 render();
-console.log('destroyButton', destroyButton);
+destroy();
+//console.log('destroyButton', destroyButton)
